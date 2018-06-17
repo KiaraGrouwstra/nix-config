a5300daa546270fb2854b750d76723ab427fc2fc
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-06-17 22:23:07 +0200

    upstream-dotfiles

--- users/tycho.nix
+++ users/tycho.nix
@@ -17,7 +17,6 @@ in
 
     shell = pkgs.fish;
 
-    group = "";
     extraGroups = [
       "audio"
       "docker"
@@ -37,11 +36,12 @@ in
   {
     # Configure various dotfiles.
     dotfiles = stringAfter [ "users" ]
-    ''
-      function sha { sudo sha256sum `realpath $1` | head -c 64; }
+    (''
+      function sha { sha256sum `realpath $1` | head -c 64; } 
       path="/etc/nixos/dotfiles"
-      for file in $(sudo find $path); do
-        local=`echo "$file" | sed -e "s|$path|/home/tycho|"`
+      i=`expr length "$path"`
+      for file in $(find $path); do
+        local="/home/tycho$'' + ''{file:$i:1000}"
         if [ -f "$file" ] && [ -f "$local" ]; then
           sha1=`sha $file`
           sha2=`sha $local`
@@ -49,22 +49,22 @@ in
             echo "matching SHA, skip: $local"
           else
             echo "differences found for $local <-> $file:"
-            diff "$file" "$local"
+            echo "sudo diff $file $local"  # not available :(
             read -p "override/upstream/skip $local? (o/u/s) " -n 1 -r
             echo # (\n)
             if [[ $REPLY =~ ^[Yy]$ ]]; then
               echo "overriding $local..."
-              sudo cp $file $local
+              cp $file $local
             elif [[ $REPLY =~ ^[Uu]$ ]]; then
               echo "upstreaming $local..."
-              sudo cp $local $file
+              cp $local $file 
             else
               echo "skipping $local..."
             fi
           fi
         fi
       done
-    '';
+    '');
   };
 
 }
