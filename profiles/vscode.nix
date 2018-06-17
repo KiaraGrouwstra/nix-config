74921ae414cc8cc8fc9764569c7f1699a5a1790a
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-06-17 14:56:25 +0200

    restore-vscode-extensions

--- profiles/vscode.nix
+++ profiles/vscode.nix
@@ -2,46 +2,41 @@
 
 let
   overlay = self: super: {
+    # TODO: fix extensions
     vscode-with-extensions = super.vscode-with-extensions.override {
       # code --list-extensions --show-versions
       # ls ~/.vscode/extensions
       vscodeExtensions =
         super.vscode-utils.extensionsFromVscodeMarketplace [
           {
-            name = "vscode-hie-server";
-            publisher = "alanz";
-            version = "0.0.19";
-            sha256 = "0x0cs7c5q90p1ffig2wb5v21z3yj3p2chgpvbnlm4gfsnw7qpfzr";
-          }
-          {
             name = "EditorConfig";
             publisher = "EditorConfig";
             version = "0.12.4";
-            sha256 = "166ia73vrcl5c9hm4q1a73qdn56m0jc7flfsk5p5q41na9f10lb0";
+            sha256 = "067mxkzjmgz9lv5443ig7jc4dpgml4pz0dac0xmqrdmiwml6j4k4";
           }
           {
             name = "vsc-material-theme";
             publisher = "Equinusocio";
             version = "2.1.0";
-            sha256 = "166ia73vrcl5c9hm4q1a73qdn56m0jc7flfsk5p5q41na9f10lb0";
+            sha256 = "1rhygrig9z1kxy0ldw28zm6xldjji53s1apxl2n9yi9vl0wn8np1";
           }
           {
             name = "material-icon-theme";
             publisher = "PKief";
             version = "3.5.0";
-            sha256 = "166ia73vrcl5c9hm4q1a73qdn56m0jc7flfsk5p5q41na9f10lb0";
+            sha256 = "0djr8dxhpf81y3x2haj7jriqsmi87may43myph3rgqmzw2s986sg";
           }
           {
             name = "Nix";
             publisher = "bbenoist";
             version = "1.0.1";
-            sha256 = "166ia73vrcl5c9hm4q1a73qdn56m0jc7flfsk5p5q41na9f10lb0";
+            sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
           }
           {
             name = "fish-vscode";
             publisher = "skyapps";
             version = "0.2.0";
-            sha256 = "166ia73vrcl5c9hm4q1a73qdn56m0jc7flfsk5p5q41na9f10lb0";
+            sha256 = "148r186y3h7n84fcyh6wa2qwl2q3pfi8aykwkc9dhfj3kwfcm5rb";
           }
         ];
         # ] ++ with super.vscode-extensions; [
@@ -55,7 +50,8 @@ in
   nixpkgs.overlays = [ overlay ];
 
   environment.systemPackages = with pkgs; [
-    vscode
+    # vscode
+    vscode-with-extensions
   ];
 
 }
