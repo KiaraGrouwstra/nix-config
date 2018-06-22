1634a977200cf188012069fe241ea5fa2392ce64
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-06-22 18:36:27 +0200

    skip firefox extensions, uBlock handled in FF Sync

--- profiles/web.nix
+++ profiles/web.nix
@@ -9,7 +9,7 @@
 
   nixpkgs.config = {
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/browsers/firefox/wrapper.nix
-    # TODO: login, ublock prebake
+    # ~/.mozilla/firefox/*.default/browser-extension-data/
     firefox = {
       jre = false;
       enableAdobeFlash = false;
