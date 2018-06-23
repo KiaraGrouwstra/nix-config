f8cda4bc5e4f0fb613dcd89e4fe901888d74b842
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-06-24 01:03:26 +0200

    python-tensorflow

--- profiles/development/python.nix
+++ profiles/development/python.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }:
 
+let
+  my-python-packages = python-packages: with python-packages; [
+    pandas
+    requests
+    tensorflow
+    tensorflow-tensorboard
+  ]; 
+  python-with-my-packages = pkgs.python3.withPackages my-python-packages;
+in
 {
   environment.systemPackages = with pkgs; [
     conda
