6fd6fa445329ccad2245b42e93c977688ca26e63
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-06-28 23:04:14 +0200

    webtorrent-cli

--- profiles/development/web.nix
+++ profiles/development/web.nix
@@ -1,12 +1,22 @@
 { pkgs, ... }:
 
+let
+  newPkgs = import (fetchTarball {
+    # 2018-05-25?
+    url = "https://github.com/NixOS/nixpkgs/archive/696c6bed4e8e2d9fd9b956dea7e5d49531e9d13f.tar.gz";
+    sha256 = "1v3yrpj542niyxp0h3kffsdjwlrkvj0mg4ljb85d142gyn3sdzd4";
+  }) {
+    config.allowUnfree = true;
+  };
+in
+
 {
   environment.systemPackages = with pkgs; [
     nodejs
     yarn
     nodePackages.npm
     nodePackages.azure-cli
-    # nodePackages.webtorrent-cli
+    newPkgs.nodePackages.webtorrent-cli
     postman
   ];
 }
