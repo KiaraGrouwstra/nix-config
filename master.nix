8751b650cef5289d11cd57b9ee99efb0a0b8f80a
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-18 08:49:57 +0200

    master-clean

--- master.nix
+++ master.nix
@@ -1,12 +1,12 @@
-# # nix-prefetch-url --unpack URL
-# let
-# rev = "1d417c39f0200bcf9eed95b428faf3e1bc0802ee";
-# in
-# import (fetchTarball {
-#   url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-#   sha256 = "0dd633kz855apa08y1k124816i10j5bg966yl955nsw0zn1zhxri";
-# }) {
-import /home/tycho/Desktop/nixpkgs {
+# nix-prefetch-url --unpack URL
+let
+rev = "aa5a93e21d77e2c2464b39a5f4dabbfebfb225b4";
+in
+import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  sha256 = "0m9fm8y7xn376bf4vnircacwfni0kr5vr6y24867didc468xyqw5";
+}) {
+# import /home/tycho/Desktop/nixpkgs {
 # import <nixpkgs> {
   config.allowUnfree = true;
 }
