2fae5feceffd60128317bb17233269b398506854
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-18 19:57:36 +0200

    earliest repro: https://github.com/NixOS/nixpkgs/pull/43682

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,8 @@
 {
 
   _module.args.pkgs = import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/5828cb3af7ee2ebe7dc460f778dc2300259ea7c4.tar.gz";
-      sha256 = "0p46vg4af884z8kpiq99zcrhq6al7rpmlm7zqwddakmrjww22lax";
+      url = "https://github.com/NixOS/nixpkgs/archive/7098b0fcdfd7fa4b82c036d8116b60b78f497316.tar.gz";
+      sha256 = "0df0y4vd10a56352q5isf0xnajimf53pn8rqm3b4kg7bsdya4cbl";
     }) {};
 
   imports = [
