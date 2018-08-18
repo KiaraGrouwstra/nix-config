c2ddc6a2e6669f8fc3b2aba8fdceea74207aa7b4
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-18 22:21:37 +0200

    minimize repro

--- configuration.nix
+++ configuration.nix
@@ -1,24 +1,9 @@
 {
-
   _module.args.pkgs = import (fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/322b2043f92b00a74246772349fd03c6c3513a15.tar.gz";
       sha256 = "0nxmfnbiymp82zrn7fd012dlwxc5mc16qyhns7vpa4q79k4qyf1d";
     }) {};
 
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  system.stateVersion = "18.03";
-
-  users.extraUsers.tycho = {
-    isNormalUser = true;
-    uid = 1000;
-    home = "/home/tycho";
-    extraGroups = ["users" "wheel"];
-  };
-
+  imports = [./hardware-configuration.nix];
 }
