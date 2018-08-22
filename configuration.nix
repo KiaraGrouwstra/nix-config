3f32718f0ac40e5d458644fe3d9419d5a71a950d
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-22 21:34:10 +0200

    remove moved settings from config

--- configuration.nix
+++ configuration.nix
@@ -6,22 +6,10 @@
   # _module.args.pkgs = import /drogon/Coding/nix/nixpkgs {
   #   config.allowUnfree = true;
   # };
-
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./configuration-common.nix
     ./machines/klink.nix
   ];
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
 
 }
