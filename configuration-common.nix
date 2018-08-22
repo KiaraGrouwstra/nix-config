c01e5e0b06cdaf9712fe42b3e50af6f17ce33d09
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-22 21:39:33 +0200

    ditch system property

--- configuration-common.nix
+++ configuration-common.nix
@@ -79,8 +79,6 @@ with lib;
     enableSSHSupport = true;
   };
 
-  system = "x86_64-linux";
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
