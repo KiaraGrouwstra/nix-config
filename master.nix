c4e61a11fdcdde1ba956a75415c730f8078f0a54
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-23 22:21:02 +0200

    2018-08-23

--- master.nix
+++ master.nix
@@ -1,10 +1,10 @@
 # nix-prefetch-url --unpack URL
 let
-  rev = "090985a443d3801f2ed04e85c8319ffe591dc96c";
+  rev = "3afef4a3da247330a3ab10f7839cbe6b6e33aaf6";
 in
 import (fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  sha256 = "0vh744hwfkgrva7p9jhcph4p1y0xb2az86xnwbag11pn6w0z6r6h";
+  sha256 = "088xwwv5m5p0hnzpvvvc013p1bnwaamn97ph9ah49fdmfajkp7ia";
 }) {
 # import /home/tycho/Desktop/nixpkgs {
 # import <nixpkgs> {
