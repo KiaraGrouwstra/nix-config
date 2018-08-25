ffc528179e70d62ac05beb45a88e512bf0695e9c
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-26 00:02:19 +0200

    commented intero

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,19 @@
+# { pkgs, ... }:
+
 {
   _module.args.pkgs = import /drogon/Coding/nix/nixpkgs {};
   boot.loader.systemd-boot.enable = true;
   nix.buildCores = 4;
   imports = [./hardware-configuration.nix];
+
+  # environment.systemPackages = with pkgs; [
+  #   # cabal-install
+  #   # stack
+  #   # ghc
+  #   # haskell.compiler.ghc822
+  #   # haskell.compiler.ghc843
+  #   haskellPackages.intero
+  # ];
+
 }
+
