b14def48eaa7c80b4e371e6254193e12b1c20f25
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-26 15:38:33 +0200

    gsconnect-daemon-procps

--- services/gsconnect.nix
+++ services/gsconnect.nix
@@ -7,7 +7,7 @@
       #  http://www.man7.org/linux/man-pages/man5/systemd.service.5.html
       Type = "simple";
       ExecStart = "${pkgs.gnome3.gjs}/bin/gjs /home/tycho/local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js";
-      ExecStop = "pkill gjs";
+      ExecStop = "${pkgs.procps}/bin/pkill gjs";
       Restart = "on-failure";
     };
     wantedBy = [ "default.target" ];
