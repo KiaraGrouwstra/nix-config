7d2ddc8e703bbce790cb9a364b274aadb0f20b69
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-26 15:10:03 +0200

    gsconnect-daemon-hacky

--- services/gsconnect.nix
+++ services/gsconnect.nix
@@ -6,7 +6,7 @@
     serviceConfig = {
       #  http://www.man7.org/linux/man-pages/man5/systemd.service.5.html
       Type = "simple";
-      ExecStart = "${pkgs.gnome3.gjs}/bin/gjs $HOME/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js";
+      ExecStart = "${pkgs.gnome3.gjs}/bin/gjs /home/tycho/local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js";
       ExecStop = "pkill gjs";
       Restart = "on-failure";
     };
