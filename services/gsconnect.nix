e6c4f35616d79b60c4c5d53b33d8bab917a75aa8
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-26 14:55:54 +0200

    gsconnect-daemon-pkgs

--- services/gsconnect.nix
+++ services/gsconnect.nix
@@ -1,10 +1,12 @@
+{ pkgs, ... }:
+
 {
   systemd.user.services.gsconnect = {
     description = "GSConnect Daemon";
     serviceConfig = {
       #  http://www.man7.org/linux/man-pages/man5/systemd.service.5.html
       Type = "simple";
-      ExecStart = "gjs ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js";
+      ExecStart = "${pkgs.gnome3.gjs}/bin/gjs ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js";
       ExecStop = "pkill gjs";
       Restart = "on-failure";
     };
