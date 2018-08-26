09910ed41de57b65182f04dbcfea3637f72fe7a2
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-08-26 15:01:19 +0200

    push iff happy path

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ if [ $1 ]; then
   sudo nixos-rebuild dry-build && \
   sudo nixos-rebuild switch && \
   git add . && \
-  git commit -am "$1"
+  git commit -am "$1" && \
   git push --all
 else
   echo "add a name!"
