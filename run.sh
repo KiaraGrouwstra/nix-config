bb6fc81cbbff851a5aec0b402be415618fe268b1
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-09-02 12:44:55 +0200

    run.sh: handle branches as profiles

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 if [ $1 ]; then
+  branch=`git rev-parse --abbrev-ref HEAD`
   sudo cp -r ./* /etc/nixos/ && \
   sudo nixos-rebuild dry-build && \
-  sudo nixos-rebuild switch && \
+  sudo nixos-rebuild switch -p $branch && \
   git add . && \
   git commit -am "$1" && \
   git push --all
