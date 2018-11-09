3afae6992e0c829a7fa42af6eb84f7e007516012
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-11-09 15:19:21 +0100

    sbt opts

--- dotfiles/shared.sh
+++ dotfiles/shared.sh
@@ -4,6 +4,3 @@
 alias torrench="docker run --rm -it jaymoulin/torrench --risky --interactive"
 alias hass="docker run --rm -it -p 8123:8123 -v $HOME/.config/homeassistant:/config homeassistant/home-assistant"
 alias gsconnect="gjs ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js"
-
-export SBT_OPTS="-Xmx1024M -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G
-
