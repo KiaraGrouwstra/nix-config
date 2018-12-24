caeddf9dfaabb81a271697a385f54947cf35fb35
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-12-24 20:45:24 +0100

    update bash script

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
-if [ $1 ]; then
-  branch=`git rev-parse --abbrev-ref HEAD`
-  sudo cp -r ./* /etc/nixos/ && \
-  sudo nixos-rebuild dry-build && \
-  sudo nixos-rebuild switch -p $branch && \
-  git add . && \
-  git commit -am "$1" # && \
-  # git push --all
-else
-  echo "add a name!"
-fi
+branch=`git rev-parse --abbrev-ref HEAD`
+sudo cp -r ./* /etc/nixos/ && \
+sudo nixos-rebuild dry-build && \
+sudo nixos-rebuild switch -p $branch && \
+git add . && \
+git commit -a
+
