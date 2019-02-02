0266389b49e14f18e75c360f6ab5a6d4bcea7d91
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-02-02 13:24:07 +0100

    brave reader alias

--- dotfiles/shared.sh
+++ dotfiles/shared.sh
@@ -4,3 +4,5 @@
 alias torrench="docker run --rm -it jaymoulin/torrench --risky --interactive"
 alias hass="docker run --rm -it -p 8123:8123 -v $HOME/.config/homeassistant:/config homeassistant/home-assistant"
 alias gsconnect="gjs ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/daemon.js"
+alias bravereader="brave --args --enable-dom-distiller"
+
