b91b2c888d52b18ae6972ccaabbf91b97e6b37c0
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-03-11 11:08:26 +0000

    script

--- users/tycho.nix
+++ users/tycho.nix
@@ -32,64 +32,64 @@ in
 
   environment.variables.DEFAULT_USER = "tycho";
 
-  # system.activationScripts =
-  # {
+  system.activationScripts =
+  {
 
-  #   # Configure various dotfiles.
-  #   dotfiles = stringAfter [ "users" ]
-  #   (''
-  #     # binDir=/run/current-system/sw/bin/
-  #     # for file in $(ls $bindir); do
-  #     #   [ ! -f $bindir$file ] && ln -s $bindir$file /bin/$file
-  #     # done
-  #     # ln -s /run/current-system/sw/bin/nvim /bin/vi
+    # Configure various dotfiles.
+    dotfiles = stringAfter [ "users" ]
+    (''
+      # binDir=/run/current-system/sw/bin/
+      # for file in $(ls $bindir); do
+      #   [ ! -f $bindir$file ] && ln -s $bindir$file /bin/$file
+      # done
+      # ln -s /run/current-system/sw/bin/nvim /bin/vi
 
-  #     function sha { sha256sum `realpath $1` | head -c 64; }
+      function sha { sha256sum `realpath $1` | head -c 64; }
 
-  #     function syncFiles {
-  #       repoPath=$1
-  #       localPath=$2
-  #       mode=$3
-  #       nixPath="/etc/nixos/$repoPath"
-  #       i=`expr length "$nixPath"`
-  #       for file in $(find $nixPath); do
-  #         local="$localPath$'' + ''{file:$i:1000}"
-  #         if [ -d "$file" ] && [ "$local" ] && [ ! -d "$local" ]; then
-  #           echo "sudo mkdir -p $local"
-  #         elif [ -f "$file" ] && [ -f "$local" ]; then
-  #           sha1=`sha $file`
-  #           sha2=`sha $local`
-  #           if [ "$sha1" != "$sha2" ]; then
-  #             if [[ $mode =~ ^up$ ]]; then
-  #               echo "sudo cp $file $local"
-  #             elif [[ $mode =~ ^down$ ]]; then
-  #               echo "sudo cp $local $file"
-  #             fi
-  #           fi
-  #         elif [ -f "$file" ] && [ ! -f "$local" ]; then
-  #           echo "sudo cp $file $local"
-  #         fi
-  #       done
-  #       echo "sudo cp -r $nixPath/. ./$repoPath/"
-  #     }
+      function syncFiles {
+        repoPath=$1
+        localPath=$2
+        mode=$3
+        nixPath="/etc/nixos/$repoPath"
+        i=`expr length "$nixPath"`
+        for file in $(find $nixPath); do
+          local="$localPath$'' + ''{file:$i:1000}"
+          if [ -d "$file" ] && [ "$local" ] && [ ! -d "$local" ]; then
+            echo "sudo mkdir -p $local"
+          elif [ -f "$file" ] && [ -f "$local" ]; then
+            sha1=`sha $file`
+            sha2=`sha $local`
+            if [ "$sha1" != "$sha2" ]; then
+              if [[ $mode =~ ^up$ ]]; then
+                echo "sudo cp $file $local"
+              elif [[ $mode =~ ^down$ ]]; then
+                echo "sudo cp $local $file"
+              fi
+            fi
+          elif [ -f "$file" ] && [ ! -f "$local" ]; then
+            echo "sudo cp $file $local"
+          fi
+        done
+        echo "sudo cp -r $nixPath/. ./$repoPath/"
+      }
     
-  #     function syncFolders {
-  #         echo
-  #         echo "###### $1 ######"
-  #         echo
-  #         syncFiles "dotfiles" "/home/tycho" "$1"
-  #         syncFiles "public" "" "$1"
-  #         syncFiles "private" "" "$1"
-  #         echo
-  #     }
+      function syncFolders {
+          echo
+          echo "###### $1 ######"
+          echo
+          syncFiles "dotfiles" "/home/tycho" "$1"
+          syncFiles "public" "" "$1"
+          syncFiles "private" "" "$1"
+          echo
+      }
 
-  #     echo
-  #     syncFolders "down"
-  #     syncFolders "up"
-  #     echo
-  #     echo "git status"
-  #     echo
-  #   '');
-  # };
+      echo
+      syncFolders "down"
+      syncFolders "up"
+      echo
+      echo "git status"
+      echo
+    '');
+  };
 
 }
