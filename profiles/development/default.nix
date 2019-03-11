4cf5e667b13795d8e2b760a2ac7c26243866e3b2
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-03-11 11:06:26 +0000

    enable dev libs

--- profiles/development/default.nix
+++ profiles/development/default.nix
@@ -6,13 +6,13 @@ in
 {
   imports = [
     # ./haskell.nix
-    # ./python.nix
-    # ./java.nix
-    # ./ruby.nix
-    # ./web.nix
-    # ./servers.nix
-    # ./compilers.nix
-    # ./latex.nix
+    ./python.nix
+    ./java.nix
+    ./ruby.nix
+    ./web.nix
+    ./servers.nix
+    ./compilers.nix
+    ./latex.nix
     # ./mathematics.nix
   ];
 
