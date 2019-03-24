fe2fe9fa9c0e8dd00c34aedadd39c2804e1cb48b
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-03-24 15:11:57 +0000

    try all desktop managers

--- profiles/os/desktop.nix
+++ profiles/os/desktop.nix
@@ -20,8 +20,32 @@
     # check current versions: nix-store -q --references /run/current-system/sw
     # try build: nix-build -A wget
     # install: nix-env -f . -iA wget
-    desktopManager.plasma5.enable = true;
-    displayManager.sddm.enable = true;
+
+    desktopManager = {
+      plasma5.enable = true;
+      gnome3.enable = true;
+      enlightenment.enable = true;
+      kodi.enable = true;
+      lumina.enable = true;
+      lxqt.enable = true;
+      mate.enable = true;
+      maxx.enable = true;
+      xfce.enable = true;
+      xterm.enable = true;
+    };
+
+    displayManager = {
+      sddm.enable = true;
+      # gdm.enable = true;
+      # lightdm.enable = true;
+      # slim.enable = true;
+      # xpra.enable = true;
+    };
+
+    windowManager = {
+      i3.enable = true;
+      # default = "i3";
+    };
 
   };
 
